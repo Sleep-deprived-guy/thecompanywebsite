6725a22f6e30354768b32f4148ffea0ee19fe452
Sleep-deprived-guy <gamergreen710@gmail.com>
2026-02-19 00:58:29 +0000

    commit m- "Added Custom Dice Roller and moved changelogs to the bottom of the page"

--- code3.py
+++ code3.py
@@ -7,3 +7,9 @@ def run_task3():
     result3 = "Rolled a D100: " + str(random.randint(1, 100))
 
     return result3
+
+def roll_custom_dice(sides):
+    if sides <= 0:
+        return "Number of sides must be positive."
+    result = f"Rolled a D{sides}: {random.randint(1, sides)}"
+    return result
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import code2
 import code3
 
 st.title("THE COMPANY website V1.0")
-st.write("V1.0 Changelog: Created website")
 
 def run_task2():
     st.session_state.result2 = None
@@ -51,3 +50,23 @@ st.button("Roll D100", on_click=run_task3)
 if st.session_state.result3 is not None:
     st.success("Rolled")
     st.write(st.session_state.result3)
+
+st.text_input("custom dice", key="dice_input")
+if st.session_state.dice_input:
+    st.button("Roll Custom Dice", on_click=st.write("press to roll again:"))
+    try:
+        sides = int(st.session_state.dice_input)
+        if sides > 0:
+            result = code3.roll_custom_dice(sides)
+            st.write(f"Rolled a D{sides}: {result}")
+        else:
+            st.error("Please enter a positive integer.")
+    except ValueError:
+        st.error("Please enter a valid integer.")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("Changelogs:")
+st.write("V1.0 Changelog: Created website")
+st.write("V1.1 Changelog: Added Custom Dice Roller and moved changelogs to the bottom of the page")
